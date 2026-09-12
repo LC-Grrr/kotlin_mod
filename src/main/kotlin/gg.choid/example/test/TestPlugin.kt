f5bf2629ac39e721d1060791e469926fe5dd27a2
grrr <len.creemers@gmail.com>
2026-09-12 16:47:49 +0200

    init

---
+++ src/main/kotlin/gg.choid/example/test/TestPlugin.kt
@@ -0,0 +1,15 @@
+package gg.choid.example.test
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TestPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        println("Hawk tuah!")
+    }
+
+    override fun onDisable() {
+
+    }
+
+}
